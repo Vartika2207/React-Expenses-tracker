f93854f995e8e379cf2f4074a29947fe71392b09
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-12-22 18:40:15 +0530

    Added react scripts

---
+++ 01-Basic-setup/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+// import React from 'react';
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
+import React from 'react';
+
+//function App() { or
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  
+  //format is (wrapper element, argument passed, 0-n txt passes)
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h3', {}, "Hello React"),
+  //   React.createElement(Expenses, {items: expenses})
+  // );
+
+
+  return (
+    <div>
+      <h3>Hello React</h3>
+      <Expenses expenses={expenses}/>
+      <Expenses expenses={expenses}/>  
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-Basic-setup/src/components/UI/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+const Card = (props) => {
+    const classess = 'card ' + props.className;
+    return (
+        <div className={classess}>{props.children}</div> //dynamic passing class 
+
+    );
+}
+
+export default Card;
+
